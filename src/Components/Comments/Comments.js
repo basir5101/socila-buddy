656015dc7469016a252fa15e1529aba7af5fc240
basir5101 <67514986+basir5101@users.noreply.github.com>
2020-09-06 18:57:12 +0600

    material ui complete

--- src/Components/Comments/Comments.js
+++ src/Components/Comments/Comments.js
@@ -1,4 +1,5 @@
 import React, { useState, useContext, useEffect } from 'react';
+import { Grid, Paper } from '@material-ui/core';
 
 const Comments = (props) => {
     const{comments, userId} = props;
@@ -10,17 +11,28 @@ const Comments = (props) => {
         .then(data => setPhoto(data.results))
     }, [])
     commentDetails.forEach(cmnt => cmnt.picture = 'https://i.imgur.com/hQI6izO.jpg')
-
+console.log(commentDetails)
     return (
         <div>
             <h4 style = {{textAlign: 'center'}}>All comments: {commentDetails.length}</h4>
           {
               
-              commentDetails.map(comment => <div className = 'comment-area'>
-                  <img src={comment.picture} alt=""/>
-                  <h4>Name: {comment.name}</h4>
-                  <p>{comment.body}</p>
-              </div>)
+              commentDetails.map(comment => <Grid container spacing={3} className = 'commentarea' container
+              direction="row"
+              justify="space-between"
+              alignItems="flex-end">
+                   <Grid item xs={3} sm={1}>
+                         <Paper>
+                            <img style = {{height: '92px'}} src={comment.picture} alt=""/>
+                         </Paper>
+                    </Grid>
+                    <Grid item xs = {12} sm = {11}>
+                        <Paper>
+                            <h5>{comment.body}</h5>
+                            <p>{comment.email}</p>
+                        </Paper>
+                    </Grid>
+              </Grid>)
           }
         </div>
         
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import Button from '@material-ui/core/Button';
+import { Grid, Paper } from '@material-ui/core';
 
 
 const Home = () => {
+    
     const [posts, setPosts] = useState([])
     useEffect(() =>{
         fetch('https://jsonplaceholder.typicode.com/posts')
@@ -14,10 +16,14 @@ const Home = () => {
     return (
         <div>
             {
-                posts.map(post => <div key = {post.id} className = 'post-area'>
-                    <h3>{post.title}</h3>
-                    <Button variant="contained" color="primary" href={'posts/'+ post.id}>See More</Button>
-                </div>)
+                posts.map(post => <Grid container spacing={3} key = {post.id} className = 'postarea'>
+                    <Grid item xs={12}>
+                        <Paper elevation={3}>
+                            <h3>{post.title}</h3>
+                            <Button variant="contained" color="primary" href={'posts/'+ post.id}>See More</Button>
+                        </Paper>                        
+                    </Grid>
+                </Grid>)
             }
         </div>
     );
